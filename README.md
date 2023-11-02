0291d16cbf3a24e9edaab3ea4107b0ebe2398001
duyvu997 <ngocduy0997@gmail.com>
2023-11-02 11:14:02 +0700

    feat(auth): add new example

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # express-init-template
 
 A simple express initial template.
+
+
+RBAC 
+ABAC
+OAuth
+OAuth2
+LDAP
+SAML
+SSO
---
+++ constant/enums.js
@@ -0,0 +1,8 @@
+export const TOKEN_ERROR_TYPE = {
+  EXPIRE: 'EXPIRE',
+};
+
+export const TOKEN_ERROR_MSG = {
+    TOKEN_EXPIRED: 'TOKEN_EXPIRED',
+    INVALID: 'INVALID',
+  };
